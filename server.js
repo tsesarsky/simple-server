3314f83d1bb204cde83d50717b7f9e08671a9d1c
Vlad Tsesarsky <v.tsesarsky@gmail.com>
2018-07-24 00:28:36 +0300

    sending different files to different requests

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const fs = require('fs');
 
 const server = http.createServer(function (request, response){
   console.log(request.method, request.url);
-  const text = fs.readFileSync('index.html', 'utf8');
-  response.end(text);
+  if (request.url == '/style.css'){
+    const css = fs.readFileSync('style.css', 'utf8');
+    response.end(css);
+  } else {
+    const html = fs.readFileSync('index.html', 'utf8');
+    response.end(html);
+  }
 });
 
 server.listen(3000);
