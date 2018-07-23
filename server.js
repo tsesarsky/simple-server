150f0f5b9b91179ffc6038fa8b4e803d05ee7bf1
Vlad Tsesarsky <v.tsesarsky@gmail.com>
2018-07-23 23:56:18 +0300

    GET/ and GET favicon.ico

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function (request, response){
-  console.log('Hello!');
+  console.log(request.method, request.url);
   const text = fs.readFileSync('index.html', 'utf8');
   response.end(text);
 });
